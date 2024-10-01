f0171abdd66953d050b80877c8c3a87b2034f304
Lerian Febriana <kanglerian@gmail.com>
2024-10-01 08:50:10 +0700

    tanpa

--- routes/schools.js
+++ routes/schools.js
@@ -6,25 +6,11 @@ const verifyapikey = require('../middlewares/verifyapikey');
 /* GET schools listing. */
 router.get('/', verifyapikey, async (req, res) => {
   try {
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
-    const offset = (page - 1) * limit;
-
     const response = await School.findAll({
       attributes: ['name','region','lat','lng','status'],
-      limit: limit,
-      offset: offset,
     });
-
-    const totalItems = await School.count();
-    const totalPages = Math.ceil(totalItems / limit);
-
     return res.status(200).json({
-      totalPages: totalPages,
-      totalItems: totalItems,
-      currentPage: page,
-      data: response.length > 0 ? response : [],
-      limit,
+      data: response
     });
   } catch (error) {
     return res.json({
