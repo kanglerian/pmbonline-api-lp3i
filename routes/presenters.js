53f4b76e5333f0f9550b3b7fec09021491d8a452
Lerian Febriana <kanglerian@gmail.com>
2024-10-01 09:04:18 +0700

    presenters

--- routes/presenters.js
+++ routes/presenters.js
@@ -6,35 +6,15 @@ const { User } = require('../models');
 /* GET presenters listing. */
 router.get('/', verifyapikey, async (req, res) => {
   try {
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
-    const offset = (page - 1) * limit;
-
-    const response = await User.findAll({
+    const data = await User.findAll({
       where: {
         role: 'P',
         status: true,
       },
-      attributes: ['identity', 'name', 'email', 'phone'],
-      limit: limit,
-      offset: offset,
+      attributes: ['identity', 'name', 'email', 'phone']
     });
 
-    const totalItems = await User.count({
-      where: {
-        role: 'P',
-        status: true,
-      },
-    });
-    const totalPages = Math.ceil(totalItems / limit);
-
-    return res.status(200).json({
-      totalPages: totalPages,
-      totalItems: totalItems,
-      currentPage: page,
-      data: response.length > 0 ? response : [],
-      limit,
-    });
+    return res.status(200).json(data);
   } catch (error) {
     return res.json({
       message: error.message
